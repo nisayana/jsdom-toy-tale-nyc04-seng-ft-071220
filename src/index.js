9c72601bf42e811455fe7100a27f1e3479f90cef
nisayana <nimaevasb@gmail.com>
2020-09-18 11:32:28 -0400

    change

--- src/index.js
+++ src/index.js
@@ -12,4 +12,57 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(toysObj => {
+    toysObj.forEach(toyObj => {
+      turnObjToHTML(toyObj)
+    })
+  })
+
+    let listOfToys = document.querySelector("div#toy-collection")
+    // console.log(listOfToys)
+    let turnObjToHTML = (toy) => {
+      let toyCard = document.createElement("div")
+      toyCard.className = "card"
+      listOfToys.append(toyCard)
+
+      let toyNameH2 = document.createElement("h2")
+      let toyImage = document.createElement("img")      
+      let toyP = document.createElement("p")
+      let toyButton = document.createElement("button")
+      toyNameH2.innerText = toy.name
+      toyImage.src = toy.image
+      toyP.innerText = toy.likes
+      toyButton.innerText = "Like"
+      toyCard.append(toyNameH2, toyImage, toyP, toyButton)
+
+    }   
+    let newToyForm = document.querySelector("form.add-toy-form")
+      newToyForm.addEventListener("submit", (event) => {
+      event.preventDefault()
+      let inputName = event.target.name.value
+      let inputImage = event.target.image.value
+      fetch(`http://localhost:3000/toys`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          name: inputName,
+          image: inputImage,
+          likes: 0
+        })
+      })
+      .then(res => res.json())
+      .then(newToyOb => {
+        turnObjToHTML(newToyOb);
+        event.target.reset()
+        })
+      })
+
+
 });
+
+
